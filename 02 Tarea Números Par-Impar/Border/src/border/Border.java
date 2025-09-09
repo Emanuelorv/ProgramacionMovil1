3699837096611ae20c64a73ffe6f71def27cf7ad
Manuelito-V <emanuel121012@gmail.com>
2025-09-09 10:17:59 -0600

    Botones y Background

---
+++ 02 Tarea Números Par-Impar/Border/src/border/Border.java
@@ -0,0 +1,15 @@
+package border;
+/**
+ *
+ * @author emanuel
+ */
+public class Border {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ 03 Botones/Manejador/src/manejador/Manejador.java
@@ -0,0 +1,12 @@
+package manejador;
+/**
+ *
+ * @author Emanuel Ortega Ventura
+ */
+public class Manejador {
+    public static void main(String[] args) {
+        Manejador2 m = new Manejador2();
+        m.setVisible(true);
+    }
+    
+}
